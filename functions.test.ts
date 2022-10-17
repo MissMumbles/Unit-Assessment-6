da03c46fd9f41a477ed871bef71b084db0896128
Kylee Warren <kyleewarren21@yahoo.com>
2022-10-16 21:38:00 -0600

    other test works? hopefully its actually a good test

--- functions.test.ts
+++ functions.test.ts
@@ -2,9 +2,13 @@ const {shuffleArray} = require('./utils')
 const{bots}= require('./data')
 
 describe('shuffleArray should return array', () => {
-    const botArr=shuffleArray(bots)
+    const shuffArr=shuffleArray(bots)
     test('should return array',()=>{
-        expect(botArr).toEqual(
+        expect(shuffArr).toEqual(
             expect.arrayContaining([]))
+    })
+    test('input array is same length as shuffled array',()=>{
+        expect(shuffArr.length).toEqual(bots.length)
+           
     })
 })
