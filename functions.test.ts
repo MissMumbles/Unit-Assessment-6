ca90228aa7d9d65dffd6bfffa8df4c5c071af78d
Kylee Warren <kyleewarren21@yahoo.com>
2022-10-16 20:31:12 -0600

    accidently deleted the first test to work. saving before continuing.

--- functions.test.ts
+++ functions.test.ts
@@ -1,5 +1,10 @@
 const {shuffleArray} = require('./utils')
+const{bots}= require('./data')
 
-describe('shuffleArray should', () => {
-    // CODE HERE
+describe('shuffleArray should return array', () => {
+    const botArr=shuffleArray(bots)
+    test('should return array',()=>{
+        expect(botArr).toEqual(
+            expect.arrayContaining([]))
+    })
 })
